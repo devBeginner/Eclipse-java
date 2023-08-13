2989e7e5b99cbafa50afaf5010699f9536d9f208
devBeginner <kyeosu@naver.com>
2023-08-13 23:50:06 +0900

    SiteTip

---
+++ Tip/src/tip1/SiteTip.java
@@ -0,0 +1,25 @@
+package tip1;
+
+public class SiteTip {
+
+	public static void main(String[] args) {
+		// 온라인 코딩 웹사이트(로그인 해야함)
+		// https://replit.com/
+
+		//   코드활용: https://www.w3schools.com/
+
+
+		// 개발환경 설정: https://www.youtube.com/watch?v=Hm2DvKtDsgs
+
+		/* 윈도우 cmd명령어
+		  dir(보유 파일)
+		  cd(이동)
+		  mkdir(폴더 새로만들기)
+		  Tab키 활용
+		  code .(vs실행)
+		 */
+		
+		
+	}
+
+}
