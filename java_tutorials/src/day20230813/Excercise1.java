aac908a49e5948abc4e1bf6b9fa8ed9739916c2d
devBeginner <kyeosu@naver.com>
2023-08-13 21:54:26 +0900

    excercise1

---
+++ java_tutorials/src/day20230813/Excercise1.java
@@ -0,0 +1,25 @@
+package day20230813;
+
+public class Excercise1 {
+
+	public static void main(String[] args) {
+		// 줄 지움 : ctrl + D
+		
+		int x = 2;
+		int y = 5;
+		
+		System.out.println(1 + x << 34); // ---------(1)
+		System.out.println(y >= 5 || x < 0 && x >= 2); //----(2)
+		System.out.println(y += 10 -  ++x); // --------(3)
+		System.out.println(x+=3);  //--------(4)
+		
+		//answer (1)12 , (2)true , (3)12 , (4)6
+		// (1) 1 + 2 = 12
+		// (2) boolean
+		// (3) 
+		// (4)
+		
+		
+	}
+
+}
